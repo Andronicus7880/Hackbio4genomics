9f38022ffbedfdfb38357dd23dc9f70c170b5898
Andronicus7880 <83078319+Andronicus7880@users.noreply.github.com>
2026-04-28 21:36:13 +0100

    Create Bacteria_data_exploratory.py

---
+++ Projects/Stage Zero /Bacteria_data_exploratory.py
@@ -0,0 +1,7 @@
+name        = "Andrew Aladele"
+affiliation = "Teesside University"
+gene        = "rbcL"
+organism    = "Spenostylis stenocarpa"
+
+print(f"""Hi, my name is {name}, a researcher at {affiliation}.
+My favorite gene is {gene} in {organism}.""")
